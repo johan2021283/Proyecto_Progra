e000bfe2f8e5ddbee242b9e35f60a644c3228b41
Sincal <sincaljulio200@gmail.com>
2022-05-19 19:40:10 -0600

    Carga Inicial

---
+++ Proyecto_Progra/src/main/java/com/mycompany/proyecto_progra/Proyecto_Progra.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto_progra;
+
+/**
+ *
+ * @author 37178
+ */
+public class Proyecto_Progra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
